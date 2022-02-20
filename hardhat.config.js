1f94b6843177ff952f674446c694c9099e99f9c7
Akshit1311 <a4748g@gmail.com>
2022-02-20 21:05:20 +0530

    update: .env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
-const secrets = require("./secrets.json");
+require("dotenv").config({ path: __dirname + "/.env.local" });
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -14,6 +13,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
+const { RPC_URL, PRIVATE_KEY } = process.env;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -22,8 +22,8 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     matic: {
-      url: secrets.rpcUrl,
-      accounts: [secrets.privkey],
+      url: RPC_URL,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
